c2c4bcd0531bf2a8d54ff531dac6f195099544be
bagjaalam1 <bagjaalam1@gmail.com>
2022-09-20 23:56:12 +0700

    addItem

--- app/controllers/penjualan.controller.js
+++ app/controllers/penjualan.controller.js
@@ -6,7 +6,7 @@ const moment = require('moment')
 exports.findPenjualan = async (req, res, next) => {
     try {
         const { rows } = await db.query('SELECT * FROM penjualan');
-        const noInvoice = req.query.noInvoice || rows.length > 0 ? rows[0].no_invoice : ''
+        const noInvoice = req.query.noInvoice ? req.query.noInvoice : rows.length > 0 ? rows[0].no_invoice : ''
         const details = await db.query('SELECT dp.*, b.nama_barang FROM detail_penjualan as dp LEFT JOIN barang as b on dp.kode_barang = b.kode_barang WHERE dp.no_invoice = $1 ORDER BY dp.id_detail', [noInvoice]);
         res.render('penjualan/list', {
             rows,
@@ -20,22 +20,43 @@ exports.findPenjualan = async (req, res, next) => {
     }
 }
 
-exports.getAdd = async (req, res, next) => {
+exports.create = async (req, res, next) => {
     try {
-        const {rows} = await db.query('SELECT kode_barang, nama_barang FROM barang')
-        res.render('penjualan/add', {
-            currentPage: 'Add Transaction',
-            barang: rows
+        const {rows} = await db.query('INSERT INTO penjualan(total_harga) VALUES (0) RETURNING *')
+        res.redirect(`/penjualan/show/${rows[0].no_invoice}`)
+    } catch (e) {
+        res.send(e)
+    }
+}
+
+exports.show = async (req, res, next) => {
+    try {
+        const penjualan = await db.query('SELECT * FROM penjualan WHERE no_invoice = $1', [req.params.no_invoice])
+        const {rows} = await db.query('SELECT kode_barang, nama_barang FROM barang ORDER BY kode_barang')
+        res.render('penjualan/form', {
+            currentPage: 'penjualan',
+            barang: rows,
+            penjualan:  penjualan.rows[0],
+            moment
         })
     } catch (e) {
         res.send(e)
     }
 }
 
-exports.start = async (req, res, next) => {
+exports.barang = async (req, res, next) => {
     try {
-        const {rows} = await db.query('INSERT INTO penjualan(total_harga) VALUES (0) RETURNING *')
-        res.json(rows[0]) //INVOICE
+        const {rows} = await db.query('SELECT * FROM barang WHERE kode_barang = $1', [req.params.kode_barang]) 
+        res.json(rows[0])
+    } catch (e) {
+        res.send(e)
+    }
+}
+
+exports.addItem = async (req, res, next) => {
+    try {
+        const {rows} = await db.query('INSERT INTO detail_penjualan(no_invoice, kode_barang, qty) VALUES ($1, $2, $3) RETURNING *', [req.body.no_invoice, req.body.kode_barang, req.body.qty]) 
+        res.json(rows[0])
     } catch (e) {
         res.send(e)
     }
--- app/routes/penjualan.routes.js
+++ app/routes/penjualan.routes.js
@@ -3,8 +3,10 @@ module.exports = (app) => {
     const router = require('express').Router();
 
     router.get('/', controller.findPenjualan);
-    router.get('/add', controller.getAdd);
-    router.post('/start', controller.start);
+    router.get('/add', controller.create);
+    router.get('/show/:no_invoice', controller.show);
+    router.get('/barang/:kode_barang', controller.barang);
+    router.post('/additem', controller.addItem);
 
     app.use('/penjualan', router);
     return router;
